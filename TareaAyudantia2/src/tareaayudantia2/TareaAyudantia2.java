048af92f810ca0c77f28116af5a12581f1f12149
yojvn <yojvn@DESKTOP-P6N3GPQ>
2015-10-11 20:09:17 -0300

    hola

--- TareaAyudantia2/src/tareaayudantia2/TareaAyudantia2.java
+++ TareaAyudantia2/src/tareaayudantia2/TareaAyudantia2.java
@@ -1,11 +1,14 @@
 package tareaayudantia2;
+
 import java.util.Scanner;
+
 /**
  *
  * @author Javier Vargas
  */
-public class TareaAyudantia2 {
 
+public class TareaAyudantia2 {
+    
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -23,49 +26,48 @@ public class TareaAyudantia2 {
         int [][] partidos = new int [2][partidosJugados];
         String [][] tabla = new String [2][partidosJugados];
         
-        for (int i=0;i<2;i++){
-            for (int j=0; j<partidosJugados;j++){
-                partidos[i][j] = (int)Math.floor(Math.random()*(0-4)+4);
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < partidosJugados; j++) {
+                partidos[i][j] = (int) Math.floor(Math.random() * (0 - 4) + 4);
                 tabla[i][j] = Integer.toString(partidos[i][j]);
             }
         }
         
-        for(int j=0;j<partidosJugados;j++){
-           if(goles>0){
-                if(partidos[0][j]==partidos[1][j]){
-                    partidos[0][j]=partidos[0][j]+1;
+        for (int j = 0; j < partidosJugados; j++) {
+           if (goles > 0) {
+                if (partidos[0][j] == partidos[1][j]){
+                    partidos[0][j] = partidos[0][j] + 1;
                     goles--;
                 }
             }
        }
         
-        for (int i=0;i<1;i++){
-            for (int j=0; j<partidosJugados;j++){
-                if(goles>0){
-                    while((partidos[i][j]<=partidos[i+1][j]) && (goles>0)){
-                        partidos[i][j]=partidos[i][j]+1;
-                        goles--;
-                    }
+        for (int j = 0; j < partidosJugados; j++) {
+            if (goles > 0) {
+                while ((partidos[0][j] <= partidos[1][j]) && (goles > 0)) {
+                    partidos[0][j] = partidos[0][j] + 1;
+                    goles--;
                 }
             }
         }
         
-        for (int j=0; j<partidosJugados;j++){
-            if(partidos[0][j]>partidos[1][j]){
+        for (int j = 0; j < partidosJugados; j++) {
+            if (partidos[0][j] > partidos[1][j]) {
                 puntos+=3;
-            }else if(partidos[0][j]==partidos[1][j]){
-                puntos+=1;
+            }else if (partidos[0][j] == partidos[1][j]) {
+                puntos += 1;
             }
         }
         
-        System.out.println("La Tabla de resultado es: \n Original - Con goles comprados");
+        System.out.println("La Tabla de resultado es: ");
+        System.out.println("Original - Con goles comprados");
         
-        for (int i=0;i<1;i++){
-            for (int j=0; j<partidosJugados;j++){
-                System.out.println(tabla[i][j]+" "+tabla[i+1][j]+"      "+partidos[i][j]+" "+partidos[i+1][j]);
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < partidosJugados; j++) {
+                System.out.print(tabla[i][j] + " " + tabla[i+1][j] + "      ");
+                System.out.println(partidos[i][j] + " " + partidos[i+1][j]);
             }
-            System.out.println(goles);
-            System.out.println(puntos);
+            System.out.println("La puntuación final es: " + puntos);
         }
     }
     
