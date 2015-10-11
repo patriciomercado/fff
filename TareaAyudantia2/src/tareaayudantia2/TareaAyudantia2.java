2b17ae7229bc7cd67ef6bd97cae2784e1ddd1bf2
yojvn <yojvn@DESKTOP-P6N3GPQ>
2015-10-11 19:50:55 -0300

    hola

--- TareaAyudantia2/src/tareaayudantia2/TareaAyudantia2.java
+++ TareaAyudantia2/src/tareaayudantia2/TareaAyudantia2.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tareaayudantia2;
 import java.util.Scanner;
 /**
@@ -11,54 +6,51 @@ import java.util.Scanner;
  */
 public class TareaAyudantia2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
         
         int puntos = 0;
+        int goles;
+        int partidosJugados;
         
         System.out.println("Cuatos partidos jugo el equipo?");
-        
-        int N = sc.nextInt();
+        partidosJugados = sc.nextInt();
         
         System.out.println("Cuatos goles compró el equipo?");
+        goles = sc.nextInt();  
         
-        int G = sc.nextInt();     
-        int [][] partidos = new int [2][N];
-        
-        String [][] tabla = new String [2][N];
+        int [][] partidos = new int [2][partidosJugados];
+        String [][] tabla = new String [2][partidosJugados];
         
         for (int i=0;i<2;i++){
-            for (int j=0; j<N;j++){
+            for (int j=0; j<partidosJugados;j++){
                 partidos[i][j] = (int)Math.floor(Math.random()*(0-4)+4);
                 tabla[i][j] = Integer.toString(partidos[i][j]);
             }
         }
         
-        for(int j=0;j<N;j++){
-           if(G>0){
+        for(int j=0;j<partidosJugados;j++){
+           if(goles>0){
                 if(partidos[0][j]==partidos[1][j]){
                     partidos[0][j]=partidos[0][j]+1;
-                    G--;
+                    goles--;
                 }
             }
        }
         
         for (int i=0;i<1;i++){
-            for (int j=0; j<N;j++){
-                if(G>0){
-                    while(partidos[i][j]<=partidos[i+1][j] && G>0){
+            for (int j=0; j<partidosJugados;j++){
+                if(goles>0){
+                    while((partidos[i][j]<=partidos[i+1][j]) && (goles>0)){
                         partidos[i][j]=partidos[i][j]+1;
-                        G--;
+                        goles--;
                     }
                 }
             }
         }
         
-        for (int j=0; j<N;j++){
+        for (int j=0; j<partidosJugados;j++){
             if(partidos[0][j]>partidos[1][j]){
                 puntos+=3;
             }else if(partidos[0][j]==partidos[1][j]){
@@ -69,10 +61,10 @@ public class TareaAyudantia2 {
         System.out.println("La Tabla de resultado es: \n Original - Con goles comprados");
         
         for (int i=0;i<1;i++){
-            for (int j=0; j<N;j++){
+            for (int j=0; j<partidosJugados;j++){
                 System.out.println(tabla[i][j]+" "+tabla[i+1][j]+"      "+partidos[i][j]+" "+partidos[i+1][j]);
             }
-            System.out.println(G);
+            System.out.println(goles);
             System.out.println(puntos);
         }
     }
