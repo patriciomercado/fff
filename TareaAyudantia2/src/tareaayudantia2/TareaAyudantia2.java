26ac0d761a2bbf3547e0fcf343335e356f4076c8
yojvn <yojvn@DESKTOP-P6N3GPQ>
2015-10-11 22:21:07 -0300

    hola

--- TareaAyudantia2/src/tareaayudantia2/TareaAyudantia2.java
+++ TareaAyudantia2/src/tareaayudantia2/TareaAyudantia2.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 /**
  *
  * @author Javier Vargas
+ * Desarrollo de la Tarea Ayudantia 2
+ * Campeonato Caridad de equipos de futbol(?)
+ * 
  */
 
 public class TareaAyudantia2 {
@@ -23,9 +26,11 @@ public class TareaAyudantia2 {
         System.out.println("Cuatos goles compró el equipo?");
         goles = sc.nextInt();  
         
+        //Inicio de las matrizes original y la modificada
         int [][] partidos = new int [2][partidosJugados];
         String [][] tabla = new String [2][partidosJugados];
         
+        //se generan numeros random entre 0 y 3 para los partidos
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < partidosJugados; j++) {
                 partidos[i][j] = (int) Math.floor(Math.random() * (0 - 4) + 4);
@@ -33,6 +38,7 @@ public class TareaAyudantia2 {
             }
         }
         
+        //se buscan partidos empatados, y se les suma un gol
         for (int j = 0; j < partidosJugados; j++) {
            if (goles > 0) {
                 if (partidos[0][j] == partidos[1][j]){
@@ -40,8 +46,9 @@ public class TareaAyudantia2 {
                     goles--;
                 }
             }
-       }
+        }
         
+        //Aca se rellenan los partidos hasta que marque como ganador
         for (int j = 0; j < partidosJugados; j++) {
             if (goles > 0) {
                 while ((partidos[0][j] <= partidos[1][j]) && (goles > 0)) {
@@ -51,6 +58,7 @@ public class TareaAyudantia2 {
             }
         }
         
+        //Se recorre la matriz para calcular el total de puntos
         for (int j = 0; j < partidosJugados; j++) {
             if (partidos[0][j] > partidos[1][j]) {
                 puntos+=3;
@@ -62,6 +70,7 @@ public class TareaAyudantia2 {
         System.out.println("La Tabla de resultado es: ");
         System.out.println("Original - Con goles comprados");
         
+        //Ultimo recorrido para imprimir los datos de ambas matrizes
         for (int i = 0; i < 1; i++) {
             for (int j = 0; j < partidosJugados; j++) {
                 System.out.print(tabla[i][j] + " " + tabla[i+1][j] + "      ");
@@ -69,6 +78,7 @@ public class TareaAyudantia2 {
             }
             System.out.println("La puntuación final es: " + puntos);
         }
+        sc.close();
     }
     
 }
