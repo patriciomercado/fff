48cdd3487d356a9c75217c157922bb86cf93edfb
patriciomercado <pato_22_to@hotmail.com>
2016-12-01 23:40:14 -0300

    01-12  mas estetico terminando lo que falta

---
+++ zzz/src/zzz/DataManager.java
@@ -0,0 +1,89 @@
+package zzz;
+
+import java.io.*;
+import java.util.ArrayList;
+
+
+
+public class DataManager {
+	
+	//lee los archivos de los jugadores y las preguntas	
+	
+	 public void crear(String nombre, String texto){
+	        
+	        File f = new File(nombre);
+	        try{
+	        FileWriter w = new FileWriter(f);
+	        BufferedWriter bw = new BufferedWriter(w);
+	        PrintWriter wr = new PrintWriter(bw);	
+	        wr.write(texto);
+	        bw.newLine();
+	       
+	        wr.close();
+	        bw.close();
+	        }catch(IOException e){};
+	        
+	 }
+	 
+         public String azarPregunta(){ 
+             ArrayList<String> preguntas = listaPreguntas();
+            int azar=(int) (Math.random()*preguntas.size()+0); 
+            return preguntas.get(azar); 
+        }
+         
+         
+	 public void agregar(String archivo, String texto){
+		    
+		    String aux =leerFichero(archivo);
+		    String cadenaIntroduciada = aux +"\n#"+texto;
+		    crear(archivo, cadenaIntroduciada);
+		}
+	 
+	 
+	
+	public ArrayList<String> listaPreguntas(){
+                //el parametro 
+		//Las preguntas estan separadas por "#" aqui se separa la cadena completa en un ArrayList
+		
+	     String ficheroPreguntas = leerFichero(leerFichero("Preguntas.txt"));
+	        String [] arrayFichero = ficheroPreguntas.split("#");
+	        ArrayList<String> preguntas = new ArrayList<>();
+	       
+	        for (int i = 0; i < arrayFichero.length; i++) {
+				preguntas.add(arrayFichero[i]);
+			}
+	       
+	       
+	        return (preguntas);
+	}
+	
+	
+	public String leerFichero(String nombre){
+		// lee un archivo y devuelve la cadena completa
+		
+		try{
+		    File f;
+		    FileReader lectorArchivo;
+		    f = new File(nombre);
+		    lectorArchivo = new FileReader(f);
+		    BufferedReader br = new BufferedReader(lectorArchivo);
+		    String l = "";
+		    String aux = "";
+		    while(true){
+		        aux = br.readLine();
+		        if(aux != null)
+		            l += "\n" + aux;
+		        else
+		            break;
+		    }
+		    br.close();
+		    lectorArchivo.close();
+		   
+		    return l;
+		}catch(IOException e){
+		System.out.println("Error:" + e.getMessage());
+		}
+		return null;
+		}
+	
+}
---
+++ zzz/src/zzz/Gam.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zzz;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author patricio mercado
+ */
+public class Gam {
+
+    public Gam() {
+    }
+    
+        private ArrayList<Player> arrayPlayers = new ArrayList<Player>();
+    
+     public int numJugadores() {
+        Object color = JOptionPane.showInputDialog(null,"Seleccione Cantidad de Jugadores",
+         "Jugadores", JOptionPane.QUESTION_MESSAGE, null,
+        new Object[] { "2", "3","4","5","6","7" },"Seleccione");
+        return Integer.parseInt(""+color);
+         }
+     
+}
